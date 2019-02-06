b0af193b5c95c810b13d7d884ea497c4f411cc33
Lauren McCoy <lmccoy8911@gmail.com>
2019-02-06 15:21:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+
+def prime? (integer)
+  if integer <= 3 || integer % 2 == 0 
+  false 
+  else
+    (2..integer).each do |element| 
+    if integer % element == 0 
+     return false   
+  else (3..integer).each do |element|
+   if integer % element != 0 
+     return true 
+ end
+ end
+ end
+ end
+ end
+ end
+ 
+ 
+
